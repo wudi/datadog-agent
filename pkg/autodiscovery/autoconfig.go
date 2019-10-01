06c6dd9d1b461306d9a553cf285bd41d82fd0d94
Albert Vaca <albert.vaca@datadoghq.com>
2019-10-01 14:07:05 +0200

    Fix crash on caused by calling /config-check too early

--- pkg/autodiscovery/autoconfig.go
+++ pkg/autodiscovery/autoconfig.go
@@ -544,10 +544,6 @@ func (ac *AutoConfig) resolveTemplateForService(tpl integration.Config, svc list
 
 // GetLoadedConfigs returns configs loaded
 func (ac *AutoConfig) GetLoadedConfigs() map[string]integration.Config {
-	if ac == nil || ac.store == nil {
-		log.Error("Autoconfig store not initialized")
-		return map[string]integration.Config{}
-	}
 	return ac.store.getLoadedConfigs()
 }
 
